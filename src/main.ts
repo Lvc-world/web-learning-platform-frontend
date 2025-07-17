7eebc9fc2ad5cd87a4dbae525df9e2108655b1dd
吕闯 <15338564108@163.com>
2025-07-17 19:03:50 +0800

    fix: git

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/display.css';
 import 'element-plus/theme-chalk/index.css';
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(ElementPlus)
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus);
 
 app.mount('#app')
